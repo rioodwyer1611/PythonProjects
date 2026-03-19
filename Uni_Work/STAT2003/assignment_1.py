c9adabcb2d6cd82cefa3040fd23e974e6fbd2b30
rioodwyer1611 <odwyerrio@outlook.com>
2026-03-20 00:25:26 +1000

    2003 assignment 4d and e answer

--- Uni_Work/STAT2003/assignment_1.py
+++ Uni_Work/STAT2003/assignment_1.py
@@ -12,6 +12,7 @@ def trial():
 N = 10**6
 sum([trial() for _ in range(N)])/N
 
+"""
 # Question 4 d)
 from math import comb
 
@@ -26,3 +27,28 @@ n = 25
 r = 75
 
 print("The outcome of n = " + str(n) + " and r = " + str(r) + " is " + str(probability_of_b(n,r)))
+
+"""
+
+# Question 4 e)
+from math import comb
+
+n = 25
+
+def probability_of_b(n, r):
+    total = 0
+    # run loop
+    for k in range(n + 1):
+        total += ((-1)**k) * comb(n, k) * ((n - k) / n)**r
+    return total
+
+def find_lowest_r(n):
+    r = 0
+    while probability_of_b(n, r) <= 0.95:
+        r += 1
+    return r
+
+lowest_r = find_lowest_r(n)
+
+print(str(lowest_r) + " is the minimal value of r. The value of P(B) at this r is " + str(probability_of_b(n, lowest_r)))
+print("To check, the value previous is " + str(probability_of_b(n, lowest_r-1)))
